438eab2f852a92bc746d7e60ffda80ff3424bba9
Bhea <bheaisipnobleza@gmail.com>
2023-04-16 22:04:46 +0800

    converted to numerical value

--- vigenere.py
+++ vigenere.py
@@ -5,3 +5,7 @@ def encrypt_plaintext(message, keyword):
 
     for i in message:
         if i.isalpha():
+        # Convert into numerical value
+            message_val = ord(A) - 65
+            key_val = ord(keyword[index]) - 65
+            index = (index + 1) % len(keyword)
